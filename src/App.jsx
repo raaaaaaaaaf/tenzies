98ced0584796e029d381f9ad66c7474d9dae04e6
Raffy Jay Ridad <rridad@ssct.edu.ph>
2023-03-27 22:26:08 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+import Die from "./components/Die";
+import './style.css'
+
+export default function App() {
+  const [dice, setDice] = useState(allNewDice())
+
+  function allNewDice() {
+    const newDice = []
+    for(let i = 0; i < 10; i++) {
+        newDice.push(Math.ceil(Math.random() * 6))
+    } 
+    return newDice
+  }
+
+  function rollDice() {
+    setDice(allNewDice())
+  }
+  
+  const diceElements = dice.map(die => <Die value={die}/>)
+  return(
+    <main>
+      <div className="dice-container">
+        {diceElements}
+
+      </div>
+      <button className="roll-dice" onClick={rollDice}>Roll
+
+      </button>
+
+    </main>
+  )
+}
